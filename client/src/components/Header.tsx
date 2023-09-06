76019f6f78cba6289ab96dbc4abb5dc5f7d70e16
Therese Linden <theresemlinden@gmail.com>
2023-09-06 09:15:33 +0200

    Added modal and cart icon.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,22 +3,48 @@ import { BsCart2 } from 'react-icons/bs';
 import { AiOutlineLogin } from 'react-icons/ai';
 import { CgProfile } from 'react-icons/cg';
 import { useState } from 'react';
+import Modal from './Modal';
+import Button from './Button';
 
 type Props = {};
 
 const Header = (props: Props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const handleCart = () => {};
   return (
-    <header>
-      <Link to={'/'}>
-        <h2>WebShop</h2>
-      </Link>
-      <div>
-        <BsCart2 />
-        {isLoggedIn ? <CgProfile /> : <AiOutlineLogin />}
-      </div>
-    </header>
+    <>
+      <header>
+        <Link to={'/'}>
+          <h2>WebShop</h2>
+        </Link>
+        <div>
+          <Button
+            Icon={BsCart2}
+            disabled={false}
+            onClick={handleCart}
+            type="button"
+          />
+
+          {isLoggedIn ? (
+            <Button Icon={CgProfile} disabled={false} type="button" />
+          ) : (
+            <Button
+              Icon={AiOutlineLogin}
+              onClick={toggleModal}
+              disabled={false}
+              type="button"
+            />
+          )}
+        </div>
+      </header>
+      {isModalOpen && <Modal toggleModal={toggleModal} />}
+    </>
   );
 };
 
