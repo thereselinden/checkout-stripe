3d321a1f2be564a93b9222b8f2e2311752258705
Therese Linden <theresemlinden@gmail.com>
2023-09-07 12:36:21 +0200

    Provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import CustomerContextProvider from './context/CustomerContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   <BrowserRouter>
-    <App />
+    <CustomerContextProvider>
+      <App />
+    </CustomerContextProvider>
   </BrowserRouter>
   // </React.StrictMode>,
 );
