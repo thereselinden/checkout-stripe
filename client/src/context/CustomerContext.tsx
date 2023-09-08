cada25cebd326decd06db09d920697c1e02f9776
Therese Linden <theresemlinden@gmail.com>
2023-09-08 10:42:41 +0200

    login, auth and logout logic

--- client/src/context/CustomerContext.tsx
+++ client/src/context/CustomerContext.tsx
@@ -1,6 +1,13 @@
-import { PropsWithChildren, createContext, useContext, useState } from 'react';
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { IRegisterForm } from '../interfaces/interfaces';
+import { ILoginForm, IUser } from '../interfaces/interfaces';
 
 const defaultValue = {
   //registerCustomer:  () => {}
@@ -13,32 +20,86 @@ export const useCustomerContext = () => useContext(CustomerContext);
 const CustomerContextProvider = ({ children }: PropsWithChildren) => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
-  const [registerSuccess, setRegisterSuccess] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState<IUser | null>(null);
 
-  const registerCustomer = async (customer: IRegisterForm): Promise<void> => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const isAlreadyLoggedIn = async (): Promise<void> => {
+      setIsLoading(true);
+      setErrorMsg(null);
+      try {
+        const response = await fetch(
+          'http://localhost:3000/api/customer/authorize',
+          {
+            credentials: 'include',
+          }
+        );
+        console.log('response', response);
+        const data = await response.json();
+        if (response.ok) {
+          setUser(data);
+          setIsLoggedIn(true);
+          setIsLoading(false);
+        } else {
+          setIsLoggedIn(false);
+          setErrorMsg(data.message);
+          setIsLoading(false);
+        }
+      } catch (err) {
+        setErrorMsg((err as Error).message);
+        setIsLoading(false);
+      }
+    };
+    isAlreadyLoggedIn();
+  }, []);
+
+  const login = async (credentials: ILoginForm): Promise<void> => {
     setIsLoading(true);
+    setErrorMsg(null);
     try {
-      const response = await fetch('http://localhost:3000/api/register', {
+      const response = await fetch('http://localhost:3000/api/customer/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(customer),
+        body: JSON.stringify(credentials),
+        credentials: 'include', // varför behöver jag denna?
       });
+
       const data = await response.json();
-      if (data) {
-        setRegisterSuccess(true);
-        setIsLoading(false);
+      if (response.ok) {
+        setUser(data);
+        setIsLoggedIn(true);
         setErrorMsg(null);
+      } else {
+        setErrorMsg(data.message);
       }
+      setIsLoading(false);
+    } catch (err: any) {
+      setErrorMsg((err as Error).message);
+      setIsLoading(false);
+    }
+  };
+
+  const logout = async (): Promise<void> => {
+    try {
+      await fetch('http://localhost:3000/api/customer/logout', {
+        method: 'POST',
+        credentials: 'include', /// varför detta?
+      });
+
+      setUser(null);
+      setIsLoggedIn(false);
+      navigate('/');
     } catch (err) {
       console.log(err);
-      setErrorMsg('Registration failed');
-      setIsLoading(false);
+      console.log((err as Error).message);
     }
   };
 
   return (
     <CustomerContext.Provider
-      value={{ registerCustomer, isLoading, errorMsg, registerSuccess }}
+      value={{ login, isLoggedIn, errorMsg, isLoading, user, logout }}
     >
       {children}
     </CustomerContext.Provider>
