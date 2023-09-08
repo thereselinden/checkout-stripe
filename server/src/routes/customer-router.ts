701d7546ee40ea59d759773c8fc4d9426f9ec027
Therese Linden <theresemlinden@gmail.com>
2023-09-08 10:45:06 +0200

    created logout and authorize endpoint

--- server/src/routes/customer-router.ts
+++ server/src/routes/customer-router.ts
@@ -1,10 +1,17 @@
-import express, { Router } from 'express';
+import express from 'express';
 
-import { register, login } from '../controllers/customer-controller';
+import {
+  register,
+  login,
+  authorize,
+  logout,
+} from '../controllers/customer-controller';
 
 const customerRouter = express.Router();
 
 customerRouter.post('/register', register);
 customerRouter.post('/login', login);
+customerRouter.post('/logout', logout);
+customerRouter.get('/authorize', authorize);
 
 export default customerRouter;
