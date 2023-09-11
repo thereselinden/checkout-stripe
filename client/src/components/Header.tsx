35befca1b081afd2263a5e3505644260ec9c374f
Therese Linden <theresemlinden@gmail.com>
2023-09-11 10:45:58 +0200

    update local state number based on global cartitem state

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,21 +3,26 @@ import { BsCart2 } from 'react-icons/bs';
 import { AiOutlineLogin } from 'react-icons/ai';
 import { BiLogOutCircle } from 'react-icons/bi';
 import { CgProfile } from 'react-icons/cg';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Modal from './Modal';
 import Button from './Button';
 import { useCustomerContext } from '../context/CustomerContext';
+import { useCartContext } from '../context/CartContext';
 import { ICartItem } from '../interfaces/interfaces';
 
 type Props = {};
 
 const Header = (props: Props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [numCartItems, setNumCartItems] = useState(0);
   const { isLoggedIn, logout } = useCustomerContext();
+  const { cartItems } = useCartContext();
 
-  let cartQuantity: number = 0;
-  const cartItems = JSON.parse(localStorage.getItem('cart'));
-  cartItems.forEach((item: ICartItem) => (cartQuantity += item.quantity));
+  useEffect(() => {
+    let cartQuantity = 0;
+    cartItems.forEach((item: ICartItem) => (cartQuantity += item.quantity));
+    setNumCartItems(cartQuantity);
+  }, [cartItems]);
 
   const toggleModal = useCallback(() => {
     setIsModalOpen(!isModalOpen);
@@ -34,6 +39,9 @@ const Header = (props: Props) => {
           <h2>WebShop</h2>
         </Link>
         <div>
+          <p className="number-cart-items">
+            {numCartItems > 0 && numCartItems}
+          </p>
           <Button Icon={BsCart2} disabled={false} type="button" />
 
           {isLoggedIn ? (
