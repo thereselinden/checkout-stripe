52a93afd57478547cefb1324409662c8637ed231
Therese Linden <theresemlinden@gmail.com>
2023-09-11 14:00:27 +0200

    controller to create checkout session

---
+++ server/src/controllers/checkout-controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { initStripe } from '../stripe/stripe';
+
+const stripe = initStripe();
+const CLIENT_URL = process.env.CLIENT_URL;
+
+export const checkoutSession = async (req: Request, res: Response) => {
+  try {
+    if (!stripe)
+      return res.status(500).json({ message: 'Could not connect to stripe' });
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: req.body.map(item => {
+        return {
+          price: item.product.default_price.id,
+          quantity: item.quantity,
+        };
+      }),
+      mode: 'payment',
+      success_url: `${CLIENT_URL}/confirmation?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: CLIENT_URL,
+    });
+
+    if (!session)
+      return res
+        .status(400)
+        .json({ message: 'Could not create checkout session' });
+
+    res.status(200).json(session);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: 'Could not create checkout session' });
+  }
+};
