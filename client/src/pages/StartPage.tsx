4226a22ac9754e16432b2e38fc0b85be8623ee7f
Therese Linden <theresemlinden@gmail.com>
2023-09-11 08:48:06 +0200

    removed test code

--- client/src/pages/StartPage.tsx
+++ client/src/pages/StartPage.tsx
@@ -1,20 +1,11 @@
 import ProductList from '../components/ProductList';
-import { useCustomerContext } from '../context/CustomerContext';
 
 type Props = {};
 
 const StartPage = (props: Props) => {
-  const { user, isLoggedIn } = useCustomerContext();
-
   return (
     <div>
-      <h1>StartPage</h1>
       <ProductList />
-      {isLoggedIn && (
-        <>
-          <p>Hello {user.firstname}</p>
-        </>
-      )}
     </div>
   );
 };
