6f65d4c35ccf2007a67a717f1644b72473ac0d07
Therese Linden <theresemlinden@gmail.com>
2023-09-11 13:16:58 +0200

    Removed unused code

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { BsCart2 } from 'react-icons/bs';
 import { AiOutlineLogin } from 'react-icons/ai';
 import { BiLogOutCircle } from 'react-icons/bi';
 import { CgProfile } from 'react-icons/cg';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Modal from './Modal';
 import Button from './Button';
 import { useCustomerContext } from '../context/CustomerContext';
@@ -13,7 +13,6 @@ import { ICartItem } from '../interfaces/interfaces';
 type Props = {};
 
 const Header = (props: Props) => {
-  // const [isModalOpen, setIsModalOpen] = useState(false);
   const [numCartItems, setNumCartItems] = useState(0);
   const { isLoggedIn, logout, isModalOpen, toggleModal } = useCustomerContext();
   const { cartItems } = useCartContext();
@@ -23,10 +22,6 @@ const Header = (props: Props) => {
     cartItems.forEach((item: ICartItem) => (cartQuantity += item.quantity));
     setNumCartItems(cartQuantity);
   }, [cartItems]);
-
-  // const toggleModal = useCallback(() => {
-  //   setIsModalOpen(!isModalOpen);
-  // }, [isModalOpen]);
 
   const handleLogout = () => {
     logout();
