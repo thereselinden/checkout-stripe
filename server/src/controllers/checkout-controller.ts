ed35a41f45955157fa7328e1f943be7828f59aaa
Therese Linden <theresemlinden@gmail.com>
2023-09-12 11:18:42 +0200

    sending customer info on checkout

--- server/src/controllers/checkout-controller.ts
+++ server/src/controllers/checkout-controller.ts
@@ -6,16 +6,18 @@ const CLIENT_URL = process.env.CLIENT_URL;
 
 export const checkoutSession = async (req: Request, res: Response) => {
   try {
+    const { cartItems, user } = req.body;
     if (!stripe)
       return res.status(500).json({ message: 'Could not connect to stripe' });
 
     const session = await stripe.checkout.sessions.create({
-      line_items: req.body.map(item => {
+      line_items: cartItems.map(item => {
         return {
           price: item.product.default_price.id,
           quantity: item.quantity,
         };
       }),
+      customer: user,
       mode: 'payment',
       success_url: `${CLIENT_URL}/confirmation?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: CLIENT_URL,
