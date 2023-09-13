5f2117cfaad93eb6470dcc882c293e5f3df7fc6d
Therese Linden <theresemlinden@gmail.com>
2023-09-13 08:52:59 +0200

    Refactor code

--- client/src/components/ProductCard/ProductCard.tsx
+++ client/src/components/ProductCard/ProductCard.tsx
@@ -2,6 +2,8 @@ import { IProduct } from '../../interfaces/interfaces';
 import { formatPrice } from '../../utils/helpers';
 import Button from '../Button/Button';
 
+import './productCard.scss';
+
 type Props = {
   product: IProduct;
   handleAddToCart: (product: IProduct, quantity: number) => void;
@@ -10,20 +12,27 @@ type Props = {
 const ProductCard = ({ product, handleAddToCart }: Props) => {
   return (
     <>
-      <article>
-        <img src={product.images[0]} alt={product.name} />
-        <h4>{product.name}</h4>
-        <p>{product.description}</p>
-        <p>
-          {formatPrice(product.default_price.unit_amount)}{' '}
-          {product.default_price.currency.toUpperCase()}
-        </p>
-        <Button
-          text="Add to cart"
-          type="button"
-          disabled={false}
-          onClick={() => handleAddToCart(product, 1)}
+      <article className="card">
+        <img
+          src={product.images[0]}
+          alt={product.name}
+          className="product-img"
         />
+        <div className="card-body">
+          <h4 className="card-title">{product.name}</h4>
+          <p>
+            {formatPrice(product.default_price.unit_amount)}{' '}
+            {product.default_price.currency.toUpperCase()}
+          </p>
+
+          <Button
+            text="Add to cart"
+            type="button"
+            disabled={false}
+            className="btn-secondary"
+            onClick={() => handleAddToCart(product, 1)}
+          />
+        </div>
       </article>
     </>
   );
