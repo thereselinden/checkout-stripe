930a3789273780f9bc7b68dff4d2b20124b5f64c
Therese Linden <theresemlinden@gmail.com>
2023-09-13 16:25:33 +0200

    Created profile path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import CartPage from './pages/CartPage/CartPage';
 
 import './styles/main.scss';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
 
 type Props = {};
 
@@ -16,7 +17,7 @@ const App = (props: Props) => {
         <Routes>
           <Route path="/" element={<StartPage />} />
           <Route path="/cart" element={<CartPage />} />
-
+          <Route path="/profile" element={<ProfilePage />} />
           <Route path="/confirmation" element={<ConfirmationPage />} />
         </Routes>
       </main>
