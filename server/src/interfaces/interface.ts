0edb8cfa3534391a3a861bb5c7b9e55b43286363
Therese Linden <theresemlinden@gmail.com>
2023-09-13 20:15:45 +0200

    test Iorder interface

--- server/src/interfaces/interface.ts
+++ server/src/interfaces/interface.ts
@@ -1,3 +1,5 @@
+import Stripe from 'stripe';
+
 export interface IUser {
   firstname: string;
   lastname: string;
@@ -11,4 +13,27 @@ export interface IUserWithoutPass {
   lastname: string;
   email: string;
   id?: string;
+}
+
+export interface ICustomer {
+  id: string | Stripe.Customer | Stripe.DeletedCustomer | null;
+  name: string | null | undefined;
+  email: string | null | undefined;
+}
+
+export interface IProduct {
+  product_id: string;
+  product_name: string;
+  product_image: string;
+  price: number;
+  quantity: number;
+  discount: number;
+  total_price: number;
+}
+export interface IOrder {
+  order_id: string;
+  created: number;
+  customer: ICustomer;
+  products: IProduct[] | undefined[];
+  amount_total: number | null;
 }
