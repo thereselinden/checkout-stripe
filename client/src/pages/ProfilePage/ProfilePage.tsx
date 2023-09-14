1757ef689039a8bd2793db88aa1f0e8b967303e2
Therese Linden <theresemlinden@gmail.com>
2023-09-14 10:13:20 +0200

    printing order info to dom

--- client/src/pages/ProfilePage/ProfilePage.tsx
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,32 +1,70 @@
 import { useEffect, useState } from 'react';
+import {
+  formatDate,
+  orderTotalPrice,
+  orderTotalQuantity,
+  totalPrice,
+} from '../../utils/helpers';
+import { useCustomerContext } from '../../context/CustomerContext';
 
 type Props = {};
 
 const ProfilePage = (props: Props) => {
-  const [orders, setOrders] = useState(null);
-  console.log('orders', orders);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [orders, setOrders] = useState<any[] | null>(null);
+  const { user } = useCustomerContext();
 
   useEffect(() => {
     const getOrders = async () => {
+      setIsLoading(true);
+      setErrorMsg(null);
       try {
         const response = await fetch('http://localhost:3000/api/orders', {
           credentials: 'include',
         });
         const data = await response.json();
         if (!response.ok) {
-          console.log('det gkci inte bra');
+          setErrorMsg(data.message);
+          setIsLoading(false);
         } else {
           setOrders(data.orders);
+          setIsLoading(false);
+          setErrorMsg(null);
         }
         console.log('data', data);
       } catch (err) {
         console.log(err);
+
+        setIsLoading(false);
       }
     };
     getOrders();
   }, []);
 
-  return <div>ProfilePage</div>;
+  return (
+    <>
+      {isLoading && <p>Getting orders....</p>}
+      {errorMsg && <p>{errorMsg}</p>}
+
+      {orders && user && (
+        <>
+          <h2>Orders</h2>
+          <h3>Hi, {user.firstname}</h3>
+
+          {orders.map(order => (
+            <section key={order.order_id} className="list">
+              <div>
+                <p>{formatDate(order.created)}</p>
+                <small>{orderTotalQuantity(order.products)} product(s)</small>
+              </div>
+              <p>{orderTotalPrice(order.products)} SEK</p>
+            </section>
+          ))}
+        </>
+      )}
+    </>
+  );
 };
 
 export default ProfilePage;
