3259b70cfb5e23020945757b1548f00c8fed8c35
Therese Linden <theresemlinden@gmail.com>
2023-09-18 13:25:52 +0200

    removed logic to empty cart here

--- client/src/pages/CartPage/CartPage.tsx
+++ client/src/pages/CartPage/CartPage.tsx
@@ -1,19 +1,19 @@
-import { useNavigate } from 'react-router-dom';
-import Button from '../../components/Button/Button';
-import { useCartContext } from '../../context/CartContext';
-import { useCustomerContext } from '../../context/CustomerContext';
-import { ICartItem } from '../../interfaces/interfaces';
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import { useCartContext } from "../../context/CartContext";
+import { useCustomerContext } from "../../context/CustomerContext";
+import { ICartItem } from "../../interfaces/interfaces";
 import {
   formatPrice,
   orderTotalQuantity,
   totalPrice,
-} from '../../utils/helpers';
+} from "../../utils/helpers";
 
-import './cartPage.scss';
+import "./cartPage.scss";
 
 const CartPage = () => {
   const { isLoggedIn, toggleModal, user } = useCustomerContext();
-  const { cartItems, setCartItems } = useCartContext();
+  const { cartItems } = useCartContext();
   const navigate = useNavigate();
 
   const handleCheckout = async () => {
@@ -24,18 +24,17 @@ const CartPage = () => {
 
     try {
       const response = await fetch(
-        'http://localhost:3000/api/checkout/create-checkout-session',
+        "http://localhost:3000/api/checkout/create-checkout-session",
         {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(cart),
         }
       );
       const data = await response.json();
       window.location.replace(data.url);
-      setCartItems([]);
     } catch (error) {
       console.log(error);
     }
@@ -50,33 +49,33 @@ const CartPage = () => {
     <>
       <h2>Your cart</h2>
       {cartItems.length > 0 ? (
-        <section className="cart-container">
-          <div className="row">
+        <section className='cart-container'>
+          <div className='row'>
             {cartItems.map((item: ICartItem) => (
               <article
                 key={item.product.id}
-                className="card cart-card col-12-xs col-4-md"
+                className='card cart-card col-12-xs col-4-md'
               >
                 <img
                   src={item.product.images[0]}
                   alt={item.product.name}
-                  className="cart-img"
+                  className='cart-img'
                 />
-                <div className="card-body">
-                  <h4 className="card-title">{item.product.name}</h4>
+                <div className='card-body'>
+                  <h4 className='card-title'>{item.product.name}</h4>
                   <p>
                     <span>Quantity:</span> {item.quantity}
                   </p>
                   <p>
-                    <span>Price:</span>{' '}
-                    {formatPrice(item.product.default_price.unit_amount)}{' '}
+                    <span>Price:</span>{" "}
+                    {formatPrice(item.product.default_price.unit_amount)}{" "}
                     {item.product.default_price.currency.toUpperCase()}
                   </p>
                 </div>
               </article>
             ))}
           </div>
-          <div className="order-summary-container col-12-xs">
+          <div className='order-summary-container col-12-xs'>
             <div>
               <h3>Order summary</h3>
               <small>{cartQuantity} products</small>
@@ -84,24 +83,24 @@ const CartPage = () => {
             <hr />
             <p>Total price: {totalPrice(cartItems)} SEK</p>
             <hr />
-            <div className="order-action">
+            <div className='order-action'>
               {!isLoggedIn ? (
                 <div>
                   <p>Login to place order</p>
                   <Button
-                    text="Log in"
+                    text='Log in'
                     disabled={false}
                     onClick={handleLogin}
-                    className="btn-secondary "
+                    className='btn-secondary '
                   />
                 </div>
               ) : (
                 <Button
-                  type="button"
-                  text="Go to Checkout"
+                  type='button'
+                  text='Go to Checkout'
                   disabled={!isLoggedIn}
                   onClick={handleCheckout}
-                  className="btn-secondary"
+                  className='btn-secondary'
                 />
               )}
             </div>
@@ -111,10 +110,10 @@ const CartPage = () => {
         <>
           <h3>Nothing here!</h3>
           <Button
-            onClick={() => navigate('/')}
-            text="Continue shopping"
+            onClick={() => navigate("/")}
+            text='Continue shopping'
             disabled={false}
-            className="btn-secondary"
+            className='btn-secondary'
           />
         </>
       )}
