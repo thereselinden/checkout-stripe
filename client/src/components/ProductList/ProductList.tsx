22263d0343bc7e31bb9c71c1592765ad81736cf9
Therese Linden <theresemlinden@gmail.com>
2023-09-21 10:00:18 +0200

    removed unused code

--- client/src/components/ProductList/ProductList.tsx
+++ client/src/components/ProductList/ProductList.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect } from "react";
 import { IProduct } from "../../interfaces/interfaces";
 import ProductCard from "../ProductCard/ProductCard";
 import { useCartContext } from "../../context/CartContext";
@@ -6,10 +6,6 @@ import CardSkeleton from "../Loader/CardSkeleton";
 import useFetch from "../../hooks/useFetch";
 
 const ProductList = () => {
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [errorMsg, setErrorMsg] = useState(null);
-  // const [products, setProducts] = useState<IProduct[] | null>(null);
-
   const {
     fetchData,
     error: errorMsg,
@@ -25,26 +21,7 @@ const ProductList = () => {
 
   useEffect(() => {
     fetchData("http://localhost:3000/api/products/", { method: "GET" });
-    // const getProducts = async () => {
-    //   setIsLoading(true);
-    //   try {
-    //     const response = await fetch("http://localhost:3000/api/products/");
-    //     const data = await response.json();
-
-    //     if (response.ok) {
-    //       setIsLoading(false);
-    //       setProducts(data.data);
-    //       setErrorMsg(null);
-    //     } else {
-    //       setIsLoading(false);
-    //       setErrorMsg(data.message);
-    //     }
-    //   } catch (err) {
-    //     setIsLoading(false);
-    //     console.log((err as Error).message);
-    //   }
-    // };
-    // getProducts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
