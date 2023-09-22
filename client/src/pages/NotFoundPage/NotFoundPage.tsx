d15a583b2fb13987c575e0bd0c209c6905c29719
Therese Linden <theresemlinden@gmail.com>
2023-09-22 08:35:07 +0200

    added some more ui informaton

--- client/src/pages/NotFoundPage/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,22 +1,23 @@
-import { useNavigate } from 'react-router-dom';
-import Button from '../../components/Button/Button';
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button";
 
-import './notFoundPage.scss';
+import "./notFoundPage.scss";
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
   const handleBackHome = () => {
-    navigate('/');
+    navigate("/");
   };
   return (
-    <section className="not-found-container">
+    <section className='not-found-container'>
       <div>
         <h1>404</h1>
         <p>Page not found</p>
+        <p>Contact service desk!</p>
       </div>
       <Button
-        text="Continue browsing"
-        className="btn-secondary"
+        text='Continue browsing'
+        className='btn-secondary'
         disabled={false}
         onClick={handleBackHome}
       />
